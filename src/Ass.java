11cf2d03cab339ab81bd5aea4e823a0b7f9b801f
ROHITH <rohith171801@gmail.com>
2024-10-15 13:47:56 +0530

    TicTacToe

--- src/Ass.java
+++ src/Ass.java
@@ -4,31 +4,33 @@ public class Ass {
         int count = 0;
         board bo = new board();
         bo.showB();
-        for (i = 0; i < 9; i++) {
+        for (i = 0; i < 10; i++) {
             if (!((bo.a[0] == bo.a[1] && bo.a[1] == bo.a[2]) || (bo.a[3] == bo.a[4] && bo.a[4] == bo.a[5])
                     || (bo.a[6] == bo.a[7] && bo.a[7] == bo.a[8]) || (bo.a[0] == bo.a[3] && bo.a[3] == bo.a[6])
                     || (bo.a[1] == bo.a[4] && bo.a[4] == bo.a[7]) || (bo.a[2] == bo.a[5] && bo.a[5] == bo.a[8])
                     || (bo.a[0] == bo.a[4] && bo.a[4] == bo.a[8]) || (bo.a[2] == bo.a[4] && bo.a[4] == bo.a[6]))
                     && (i % 2 == 0)) {
                 bo.PutX();
-                count = 2;
+                count++;
             } else if (!((bo.a[0] == bo.a[1] && bo.a[1] == bo.a[2]) || (bo.a[3] == bo.a[4] && bo.a[4] == bo.a[5])
                     || (bo.a[6] == bo.a[7] && bo.a[7] == bo.a[8]) || (bo.a[0] == bo.a[3] && bo.a[3] == bo.a[6])
                     || (bo.a[1] == bo.a[4] && bo.a[4] == bo.a[7]) || (bo.a[2] == bo.a[5] && bo.a[5] == bo.a[8])
                     || (bo.a[0] == bo.a[4] && bo.a[4] == bo.a[8]) || (bo.a[2] == bo.a[4] && bo.a[4] == bo.a[6]))
                     && (i % 2 != 0)) {
-                bo.PutO();
-                count = 4;
-            } else{
+                if (i < 9) {
+                    bo.PutO();
+                }
+                count++;
+            } else {
                 break;
             }
 
         }
-        if (i == 9) {
-            System.out.println("Draw :(");
-        } else if (count == 2) {
+        if (count % 2 != 0) {
             System.out.println("Player X wins :)");
-        } else if (count == 4) {
+        } else if (count == 10) {
+            System.out.println("Draw :(");
+        } else if (count % 2 == 0) {
             System.out.println("Player O wins :)");
         }
     }
